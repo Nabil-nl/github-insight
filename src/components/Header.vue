9643efbfd4fe5f1adca13ff6970af78eaa535442
Nabil-nl <nabilouhmida9@gmail.com>
2025-02-02 23:03:42 +0100

    upadte

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,30 +1,24 @@
-<template> 
-<head class="flex justify-center items-center">
-  <div class="fixed left-[600px] top-[-14px] mr-4 mt-4">
-    <div class="card border-4 border-[#87bcde]">
-      <div class="bg"> 
-        <span class="add text-[#f0f4ef] flex justify-center items-center">Github Insights</span>
+<template>
+  <header class="flex justify-center items-center">
+    <div class="fixed left-[600px] top-[-14px] mr-4 mt-4">
+      <div class="card border-4 border-[#87bcde]">
+        <div class="bg"> 
+          <span class="add text-[#f0f4ef] flex justify-center items-center">GitHub Insights</span>
+        </div>
+        <div class="blob"></div>
       </div>
-      <div class="blob"></div>
     </div>
-  </div>
-</head>
-
-
-
-
+  </header>
 </template>
-
-
 
 <script>
 import '@/css/header.css';
 
 export default {
-    name: 'Header',
-    
+  name: 'Header',
 }
 </script>
-<style scoped>
 
+<style scoped>
+/* You can add any component-specific styles here */
 </style>
